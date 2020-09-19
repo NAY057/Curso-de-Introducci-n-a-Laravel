7fd297fb87e2c1b22a77a8114e7cfdce6fbbe98d
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2020-09-19 13:32:05 -0500

    new form added

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,13 +11,41 @@
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <!-- Styles -->
-
+        {{-- <link href="{{ asset('/css/stylesheet.css') }}" rel="stylesheet"/> --}}
+        {{-- <link href="{{ asset('/css/bootstrap.min/css') }} rel="stylesheet"/> --}}
+        {{-- <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> --}}
+        {{-- <link href="{{ asset('/css/stylesheet.css') }}" rel="stylesheet"/> --}}
+        {{-- <link rel="stylesheet" href="/bootstrap/css/bootstrap.css" media="screen"> --}}
+        {{-- <link href=" {{ URL::asset('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
     </head>
     <body >
         <div class="container">
             <div class="row">
-                <div class="col-sm-15 mx-auto">
-                    <table>
+                <div class="col-xl-8 mx-auto">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            <form action="{{ route('user.store') }}" method="POST">
+                                <div class="form-row">
+                                    <div class="col-sm-3">
+                                        {{-- EL 'name' HACE REFERENCIA AL NOMBRE QUE TIENEN LOS ATRIBUTOS EN LA BASE DE DATOS. --}}
+                                        <input type="text" name="name" class="form-control" placeholder="Nombre">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="email" class="form-control" placeholder="Email">
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                                    </div>
+                                    <div class="col-auto">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Enviar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    {{--LA TABLA NO DEBE DE SER UN DIV --}}
+                    <table class="table">
                         {{-- Encabezado de la tabla --}}
                         <div class="thead-dark">
                             <tr>
